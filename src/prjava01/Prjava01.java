e1b1cdb41ee6b9bdba8f30521394c1aa073bc9f4
david <david@oO>
2014-04-15 10:26:07 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -29,7 +29,7 @@ public class Prjava01 {
             bw.newLine();
             bw.write("<body>");
             bw.newLine();
-            bw.write("Novap&agrave;ginaweb");
+            bw.write("<h1>DAW2m08uf4pr2</h1>");
             bw.newLine();
             bw.write("</body>");
             bw.newLine();
